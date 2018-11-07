1f0faf085bf4fe7b2479dde011974f03f2fcce59
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-06 21:59:46 -0800

    add jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App Component
+        <div className="jumbotron">
+            <h1 className="display-4">Welcome to Meenu's Portfolio!</h1>
+            <hr className="my-4" />
+            <p className="lead">
+                <Link to="/projects" className="btn btn-primary btn-lg" role="button">Check Out My Projects!</Link>
+            </p>
+          </div>
       </div>
     );
   }
