000645e711ed6b5f9678f7373d35216983fb5889
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-12 10:26:36 -0800

    edit App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="jumbotron">
+        <Route exact path="/" render={() => (
+          <div className="jumbotron">
             <h1 className="display-4">Welcome to Meenu's Portfolio!</h1>
             <hr className="my-4" />
             <p className="lead">
-                <Link to="/projects" className="btn btn-primary btn-lg" role="button">Check Out My Projects!</Link>
+                <Link to="/" className="btn btn-primary btn-lg" role="button">Check It Out!</Link>
             </p>
           </div>
+        )} />
       </div>
     );
   }
